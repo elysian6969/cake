3b86d99933b5338a0aa6ceb62005d40788e3f096
ely <tsujianii@tuta.io>
2022-07-18 05:02:34 +0000

    improve tuple example

---
+++ examples/from_tuple.rs
@@ -0,0 +1,19 @@
+#![feature(type_name_of_val)]
+
+use cake::{array, slice};
+use core::any;
+
+fn main() {
+    let tuple = ("hi", "world");
+    let array = array::from_tuple_ref(&tuple);
+
+    println!("array: {} = {array:?}", any::type_name_of_val(&array));
+
+    let slice = slice::from_tuple_ref(&tuple);
+
+    println!("slice: {} = {slice:?}", any::type_name_of_val(&slice));
+    
+    let array = array::from_tuple(tuple);
+    
+    println!("array: {} = {array:?}", any::type_name_of_val(&array));
+}
